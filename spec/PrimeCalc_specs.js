1f191bb02f40621699128c958325c6b0c0b14c0d
steven2805 <stevenmac90@googlemail.com>
2018-01-17 14:45:55 +0000

    Testing can create a range 0..100

--- spec/PrimeCalc_specs.js
+++ spec/PrimeCalc_specs.js
@@ -4,13 +4,11 @@ var PrimeCalculator = require("../public/PrimeCalc.js");
 describe('This is testing the Prime Calculator', function(){
 
 
-    it('should return 20', function () {
-        var primeCalc = new PrimeCalculator(20);
-        var value = primeCalc.calc();
-        assert.deepEqual(value,40);
-
-
+    it('should return 100 values', function () {
+        var primeCalc = new PrimeCalculator(100);
+        var value = primeCalc.range;
+        assert.deepEqual(value.length,100);
     });
 
 
-})
+});
