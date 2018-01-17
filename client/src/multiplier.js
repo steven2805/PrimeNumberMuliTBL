d65ce61f991583f0d58b41995073974b725a214a
steven2805 <stevenmac90@googlemail.com>
2018-01-17 22:03:00 +0000

    multiploer passing test

--- client/src/multiplier.js
+++ client/src/multiplier.js
@@ -1,12 +1,25 @@
 
 
 function Multiplier(primes){
-    this.primes = primes
+    this.output = runMultiplier(primes)
 }
 
-runMultiplier = function(){
+runMultiplier = function(primes){
+    console.log(primes);
+    var copyPrimes = primes.slice(0);
 
-}
+    var valueArray = [];
+    for(var i = 0; i < primes.length; i++){
+        console.log(i);
+        var innerValues = [];
+        for(var j = 0; j < primes.length;j++){
+            innerValues.push(primes[i] * primes[j]);
+        }
+        valueArray.push(innerValues);
+    }
+    return valueArray;
+
+};
 
 
 
--- spec/Multiplier_specs.js
+++ spec/Multiplier_specs.js
@@ -6,8 +6,9 @@ describe('Should be testing the multiplier functions', function(){
 
 
     it('should take one value from array and multiply it with all other numebrs including self', function () {
-        var muli = new Multiplier([1,2,3,4,5]);
-        assert.deepEqual(multi.output)
+        var multi = new Multiplier([2,3,5]);
+        var values = [[4,6,10],[6,9,15],[10,15,25]];
+        assert.deepEqual(multi.output, values)
     });
 
 
